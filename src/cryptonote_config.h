04208fdcd3233d864a9aedb2b1cc57c96b7bc517
akildemir <akildemir72@gmail.com>
2025-05-12 15:50:08 +0300

    compile commits

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -263,6 +263,7 @@
 #define BULLETPROOF_PLUS_MAX_OUTPUTS            16
 
 #define FCMP_PLUS_PLUS_MAX_INPUTS               8
+#define FCMP_PLUS_PLUS_MAX_OUTPUTS              8
 
 #define CRYPTONOTE_PRUNING_STRIPE_SIZE          4096 // the smaller, the smoother the increase
 #define CRYPTONOTE_PRUNING_LOG_STRIPES          3 // the higher, the more space saved
