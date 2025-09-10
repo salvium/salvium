c005145cb2ecba14bbe71daa83238eb12f900a8c
auruya <dream.glorix@gmail.com>
2025-09-10 19:43:06 +0300

    add stake transaction functional test

--- tests/functional_tests/stake_tx.h
+++ tests/functional_tests/stake_tx.h
@@ -28,11 +28,7 @@
 // 
 // Parts of this file are originally copyright (c) 2012-2013 The Cryptonote developers
 
-std::string generate_random_wallet_name();
-
-bool transactions_flow_test(std::string& working_folder, 
-                            std::string path_source_wallet, 
-                            std::string path_target_wallet, 
-                            std::string& daemon_addr_a, 
-                            std::string& daemon_addr_b, 
-                            uint64_t amount_to_transfer, size_t mix_in_factor, size_t transactions_count, size_t transactions_per_second);
+bool stake_transaction_test(std::string& working_folder,
+  std::string wallet_name,
+  std::string& daemon_addr_a,
+  uint64_t amount_to_stake);
