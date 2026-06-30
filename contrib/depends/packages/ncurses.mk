5c0154e287b81bf2b200a30df86d518480abb06a
auruya <dream.glorix@gmail.com>
2026-06-30 22:16:38 +0300

    correct ncurses termlib name on darwin

--- contrib/depends/packages/ncurses.mk
+++ contrib/depends/packages/ncurses.mk
@@ -3,7 +3,7 @@ $(package)_version=6.1
 $(package)_download_path=https://ftp.gnu.org/gnu/ncurses
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=aa057eeeb4a14d470101eff4597d5833dcef5965331be3528c08d99cebaa0d17
-$(package)_patches=fallback.c gcc15-source-warnings.patch
+$(package)_patches=fallback.c gcc15-source-warnings.patch darwin-termlib-name.patch
 
 define $(package)_set_vars
   $(package)_build_opts=CC="$(firstword $($(package)_cc))"
@@ -47,6 +47,7 @@ endef
 define $(package)_preprocess_cmds
   cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub . && \
   patch -p1 < $($(package)_patch_dir)/gcc15-source-warnings.patch &&\
+  patch -p1 < $($(package)_patch_dir)/darwin-termlib-name.patch &&\
   cp $($(package)_patch_dir)/fallback.c ncurses
 endef
 
@@ -62,7 +63,7 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE) -j1 MAKEFLAGS= $($(package)_build_opts) V=1
+  $(MAKE) $($(package)_build_opts) V=1
 endef
 
 define $(package)_stage_cmds
